7c3cffdc226d297a9d8ffcc249abab3096338cd4
Oleg Guzey <oguzey@cybervisiontech.com>
2015-03-03 21:10:42 +0200

    Refactoring of Signature.py

--- signature/Signature.py
+++ signature/Signature.py
@@ -12,61 +12,32 @@ parser.add_option("-r", "--report", dest="report", help="Path to a file with rep
 parser.add_option("-a", "--action", dest="action", help="A action which will be executed (calc, check)")
 (options, args) = parser.parse_args()
 
-if options.action == "check":
-	pass
-	# if options.report is None:
-	# 	print "The file with report must be provided (see help)"
-	# 	exit()
-elif options.action == "calc":
-	if options.report is None:
-		options.report = "report.txt"
-		print "Creating file for report ({0}/{1})\n".format(os.getcwd(), options.report)
-else:
-	print "Unrecognized command. (see help)"
-	exit()
-
+##########################################################
 #
 #  Caclulation...
 #
 ##########################################################
-def get_signature(path):
-
+def get_common_parameters():
 	p = int("AF5228967057FE1CB84B92511BE89A47", 16)
 	a = int("9E93A4096E5416CED0242228014B67B5", 16)
 	q = int("57A9144B382BFF0E5C25C9288DF44D23", 16)
+	return (a, q, p)
+
+def get_signature(path):
+	a, q, p = get_common_parameters()
+	hash = hashModule.get_hash(path)
+	H = format(hash, "016x") + "00ffffffffffff00"
+	H = re.findall('..', H)
+	H = "".join(reversed(H))
+	H = int(H, 16)
 
 	x = int(os.urandom(16).encode('hex'), 16)
 	x = x % (p + 1)
 	y = pow(a, x, p)
 
-	print "p is {0}".format(format(p, "032x"))
-	print "a is {0}".format(format(a, "032x"))
-	print "q is {0}".format(format(q, "032x"))
-	print "x is {0}".format(format(x, "032x"))
-	print "y is {0}".format(format(y, "032x"))
-
-
-
-	hash = hashModule.get_hash(path)
-	str_hash = format(hash, "016x")
-	print "hash is {0}".format(str_hash)
-
-	H = str_hash + "00ffffffffffff00"
-	H = re.findall('..', H)
-	H = "".join(reversed(H))
-	H = int(H, 16)
-
-	print "H is {0}".format(format(H, "032x"))
-
 	U = int(os.urandom(16).encode('hex'), 16)
 
-	U = int("1458DA6E9B624A5D2999A200E88C3842", 16)
-
-	print "U is {0}".format(format(U, "032x"))
-
 	Z = pow(a, U, p)
-
-	print "Z is {0}".format(format(Z, "032x"))
 
 	Z_sh = (Z * H ) % p
 	g = (x * Z * pow(Z_sh, q-2, q) ) % q
@@ -75,64 +46,110 @@ def get_signature(path):
 
 	S = pow(a, g, p)
 
+	print "\nCaclulation hash ..."
+	print "#" * 40
+	print "p is {0}".format(format(p, "032x"))
+	print "a is {0}".format(format(a, "032x"))
+	print "q is {0}".format(format(q, "032x"))
+	print "x is {0}".format(format(x, "032x"))
+	print "y is {0}".format(format(y, "032x"))
+	print "H is {0}".format(format(H, "032x"))
+	print "U is {0}".format(format(U, "032x"))
+	print "Z is {0}".format(format(Z, "032x"))
 	print "g is {0}".format(format(g, "032x"))
 	print "k is {0}".format(format(k, "032x"))
 	print "S is {0}".format(format(S, "032x"))
+	print "#" * 40
 
-	return {"H" : H, "K": k, "S": S, "Y": y}
+	return {"H" : H, 
+			"K": k, 
+			"S": S, 
+			"Y": y}
+
+
 ##########################################################
-
 #
 #   Checking...
 #
 ##########################################################
-def check():
-	print "\nChecking..."
-	print "=" * 40
-	expo = (H * S * pow(a, k, p)) % p
-	left = pow(S, expo, p)
+def verification_signature(data):
+	a, q, p = get_common_parameters()
+	print "\nVerification to correct calculation..."
+	print "#" * 40
+	common = (data["S"] * pow(a, data["K"], p)) % p
+
+	left = pow(data["S"], (data["H"] * common) % p, p)
 	print "left is  {0}".format(format(left, "032x"))
 
-	expo = (S * pow(a, k, p)) % p
-	right = pow(y, expo, p)
+	right = pow(data["Y"], common, p)
 	print "right is {0}".format(format(right, "032x"))
 
-	print "=" * 40
+	print "#" * 40
+	return left == right
+
+
 ##########################################################
 
 #
 #	Writing/reading to file...
 #
 ##########################################################
-if options.action == "calc":
-	sign = get_signature(options.data)
-	with open(options.report, "w+") as f:
-		f.write("{0}/{1}\n".format(os.getcwd(), options.data))
+def write_report(sign, path_report, path_orig):
+	with open(path_report, "w+") as f:
+		f.write("{0}/{1}\n".format(os.getcwd(), path_orig))
 		f.write("H = {0}\n".format(format(sign["H"], "032x")))
 		f.write("Y = {0}\n".format(format(sign["Y"], "032x")))
 		f.write("K = {0}\n".format(format(sign["K"], "032x")))
 		f.write("S = {0}\n".format(format(sign["S"], "032x")))
-else:
-	text = None
+
+
+def read_parse_report(path_report):
+	result = {}
 	path = None
-	with open(options.data, "r") as f:
+	text = None
+	with open(path_report, "r") as f:
 		path = f.readline()
 		text = f.read()
-	path = path[:len(path) - 1]
-
-	sign = get_signature(path)
+	result["path"] = path.strip('\n')
 	check_sig = re.compile(r"H\s*=\s*(?P<H>[0-9A-Fa-f]+)\s*Y\s*=\s*(?P<Y>[0-9A-Fa-f]+)\s*"
 					"K\s*=\s*(?P<K>[0-9A-Fa-f]+)\s*S\s*=\s*(?P<S>[0-9A-Fa-f]+)",
 				0).search(text).groupdict()
-	print "origin sign => "
-	print format(sign["H"], "032x")
-	print format(sign["S"], "032x")
-	print format(sign["K"], "032x")
 
-	print "read sign ==> "
-	print check_sig
+	result.update((key, int(value, 16)) for key, value in check_sig.items())
+	return result
 
-	if sign["S"] == int(check_sig["S"], 16) and sign["k"] == int(check_sig["K"], 16):
-		print "Sign is correct"
-    #print "Result of calculation was saved to {0}/{1}".format(os.getcwd(), options.report)
 ##########################################################
+
+if options.action == "calc":
+	if options.report is None:
+		options.report = "report.txt"
+		print "Creating file for report ({0}/{1})".format(os.getcwd(), options.report)
+	if options.data is None:
+		print "File with data must be provided"
+		exit()
+
+	sign = get_signature(options.data)
+	verification_signature(sign)
+	write_report(sign, options.report, options.data)
+	print "\nSignature was successful write to {0}/{1}".format(os.getcwd(), options.report)
+
+elif options.action == "check":
+	if options.data is None:
+		print "File with data must be provided"
+		exit()
+	data = read_parse_report(options.data)
+	sign = get_signature(data["path"])
+	
+	if data["H"] == sign["H"] and verification_signature(data) is True:
+		print "\nSignature is correct."
+	else:
+		print "\nSignature is incorrect."
+		print "Read data"
+		for x, y in data.items():
+			if x != "path":
+				print "\t{0} is {1}".format(x, format(y, "032x"))
+		print "Caclulated signature"
+		for x, y in sign.items():
+			print "\t{0} is {1}".format(x, format(y, "032x"))
+else:
+	print "Unrecognized command. (see help)"
